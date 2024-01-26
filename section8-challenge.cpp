877d001d293d52a7dad1e7b282ecec0a234ce168
mpflynnx <mpflynnx@gmail.com>
2024-01-26 08:46:54 +0000

    #13 My solution using % operator

--- section8-challenge.cpp
+++ section8-challenge.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+int func(int amount, int type)
+{
+    if (amount >= type)
+    {
+        int num_of_type {amount / type};
+        return num_of_type;
+    }
+
+    return 0;
+}
+
 int main()
 {
     const int dollar {100};
@@ -14,66 +25,20 @@ int main()
 
     std::cout << "\nYou can provide this change as follows:\n";
 
-    int remainder {0}; 
-    int num_of_dollars {0};
-    if (amount >= dollar)
-    {
-        num_of_dollars = amount / dollar;
-        // std::cout << "amount remainder: " << remainder << '\n'; 
-    }
+    std::cout << "dollars    : "  << func(amount, dollar) << '\n';
+    amount %= dollar;
 
-    std::cout << "dollars    : "  << num_of_dollars << '\n';
-    remainder = amount %= dollar;
+    std::cout << "quarters : " << func(amount, quarter) << '\n';
+    amount %= quarter;
 
-    int num_of_quarters {0};
-    if (amount >= quarter)
-    {
-        num_of_quarters = amount / quarter;
-        // std::cout << "amount remainder: " << remainder << '\n'; 
-    }
-    std::cout << "quarters : " << num_of_quarters << '\n';
-    remainder = amount %= quarter;
+    std::cout << "dimes     : " << func(amount, dime) << '\n';
+    amount %= dime;
 
-    int num_of_dimes {0};
-    if (amount >= dime)
-    {
-        num_of_dimes = amount / dime;
+    std::cout << "nickels   : " << func(amount, nickel) << '\n';
+    amount %= nickel;
 
-        // std::cout << "amount remainder: " << remainder << '\n'; 
-    }
-    std::cout << "dimes     : " << num_of_dimes << '\n';
-    remainder = amount %= dime;
-
-    int num_of_nickels {0};
-    if (amount >= nickel)
-    {
-        num_of_nickels = amount / nickel;
-
-        // std::cout << "amount remainder: " << remainder << '\n'; 
-    }
-    std::cout << "nickels   : " << num_of_nickels << '\n';
-    remainder = amount %= nickel;
-
-    int num_of_pennies {0};
-    if (amount >= penny)
-    {
-        num_of_pennies = amount / penny;
-
-        // std::cout << "amount remainder: " << remainder << '\n'; 
-    }
-
-    std::cout << "pennies  : " << num_of_pennies << '\n';
-    remainder = amount %= penny;
-
-    // std::cout << "\nYou can provide this change as follows:\n";
-    // std::cout << "dollars    : " << num_of_dollars << '\n';
-    // std::cout << "quarters : " << num_of_quarters << '\n';
-    // std::cout << "dimes     : " << num_of_dimes << '\n';
-    // std::cout << "nickels   : " << num_of_nickels << '\n';
-    // std::cout << "pennies  : " << num_of_pennies << '\n';
+    std::cout << "pennies  : " << func(amount, penny) << '\n';
+    amount %= penny;
 
 	return 0;
 }
-
-// is amount >= 100
-// what is the remainder of amount % 100
